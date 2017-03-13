caa33417671dc6b3144c549d8b83a9aab5b08ad3
Robin Burchell <robin.burchell@crimson.no>
2017-03-13 14:03:28 +0100

    Schema: Change for the new dashboard format
    Primary changes:
    * We store all benchmark results in a 'benchmarks' measurement, keyed by
      a 'benchmark' tag
    * We remove all version information (OS and Qt). The OS and hardware
      versions are really most useful through the hardware identifier anyway,
      so this doesn't add much useful information. The Qt version isn't
      really all that useful either: for releases, we can create
      annotations, and for everything else, we have the branch identifier
      which is more accurate than the version number anyway in some ways.

--- run.py
+++ run.py
@@ -10,44 +10,34 @@ import json
 HOSTNAME = "10.213.255.45:8086"
 #HOSTNAME = "localhost:8086"
 
-def submit_output(output, branch, qtBaseHead, qtDeclarativeHead):
+def submit_output(output, branch, hardwareId):
     tree = json.loads(output)
-    gpuVendor = tree["opengl"]["vendor"]
-    driverVersion = tree["opengl"]["version"]
-    qtVersion = tree["qt"]
-    osVersion = tree["os"]["prettyProductName"].replace(' ', '.')
     for key in tree:
         if key.endswith(".qml"):
             mean = tree[key]["average"]
             standardDeviation = tree[key]["standard-deviation-all-samples"]
             coefficientOfVariation = standardDeviation / mean if mean > 0.0 else 0.0
             basename = key.split("/")[-1]
-            tags = ('qtVersion=' + qtVersion, 'osVersion=' + osVersion, 'branch=' + branch)
+            tags = ('branch=' + branch, 'benchmark=' + basename, 'hardwareId=' + hardwareId)
             fields = ('mean=' + str(mean),
                       'coefficientOfVariation=' + str(coefficientOfVariation),
-                      'qtBaseHead="' + qtBaseHead + '"',
-                      'qtDeclarativeHead="' + qtDeclarativeHead + '"',)
+                      )
 
-            data = '%s,%s %s' % (basename, ','.join(tags), ','.join(fields))
+            data = 'benchmarks,%s %s' % (','.join(tags), ','.join(fields))
             result = requests.post("http://%s/write?db=qmlbench" % HOSTNAME, data=data.encode('utf-8'))
             print(data)
             print(result)
 
-    
-def run_benchmark(filename, branch, qtBaseHead, qtDeclarativeHead):
+def run_benchmark(filename, branch, hardwareId):
     output = subprocess.check_output(["cat", filename])
-    submit_output(output.decode("utf-8"), branch, qtBaseHead, qtDeclarativeHead)
-    
-    
+    submit_output(output.decode("utf-8"), branch, hardwareId)
+
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help = "The .json file to post")
     parser.add_argument("branch", help = "The Qt branch tested")
-    parser.add_argument("qtBaseHead", help = "The current HEAD of Qt Base")
-    parser.add_argument("qtDeclarativeHead", help = "The current HEAD of Qt Declarative")
+    parser.add_argument("hardwareId", help = "Our unique hardware ID (e.g. linux_imx6_eskil)")
     args = parser.parse_args(sys.argv[1:])
     print("Posting results: " + args.filename)
-    run_benchmark(args.filename, args.branch, args.qtBaseHead, args.qtDeclarativeHead)
-    
-    
+    run_benchmark(args.filename, args.branch, args.hardwareId)
