3113d34660d9e5546acde4ee3297c1a3e250fe73
Daniel Smith <daniel.smith@qt.io>
2020-04-13 11:03:43 +0200

    Clean up qttools directory on builds and properly format script

--- build_and_test_windows.ps1
+++ build_and_test_windows.ps1
@@ -1,11 +1,11 @@
 
 param (
-	[Parameter(Mandatory=$true)][string]$QtVersion = "dev",
-	[Parameter(Mandatory=$true)][string]$MachineName = "UNKNOWN",
-	[int]$BuildCores = 2,
-	[switch]$Annotate,
-	[string]$QtDeclarativeVersion = "",
-	[string]$FlexBisonDir = "$PWD\flex_bison\"
+    [Parameter(Mandatory = $true)][string]$QtVersion = "dev",
+    [Parameter(Mandatory = $true)][string]$MachineName = "UNKNOWN",
+    [int]$BuildCores = 2,
+    [switch]$Annotate,
+    [string]$QtDeclarativeVersion = "",
+    [string]$FlexBisonDir = "$PWD\flex_bison\"
 )
 
 
@@ -13,45 +13,46 @@ function checkoutQtModule([string]$module, [string]$version) {
     git clone --progress https://code.qt.io/qt/$module
     cd $module
     git checkout $version
-    git rev-parse HEAD > ([string]::Format("../{0}_{1}_sha1.txt",$module,$version))
+    git rev-parse HEAD > ([string]::Format("../{0}_{1}_sha1.txt", $module, $version))
     cd ..
 }
 
 function buildQtModule([string]$module, [string]$version, [int]$BuildCores) {
     checkoutQtModule $module $version
     cd $module
-	if ($module -eq "qttools"){
-		cd src/windeployqt
-    ../../../qtbase/bin/qmake
-    ../../../qmlbenchrunner/JOM/jom.exe -j $BuildCores
-	cd ../..
-	} else {
-    ../qtbase/bin/qmake
-    ../qmlbenchrunner/JOM/jom.exe -j $BuildCores
-	}
+    if ($module -eq "qttools") {
+        cd src/windeployqt
+        ../../../qtbase/bin/qmake
+        ../../../qmlbenchrunner/JOM/jom.exe -j $BuildCores
+        cd ../..
+    }
+    else {
+        ../qtbase/bin/qmake
+        ../qmlbenchrunner/JOM/jom.exe -j $BuildCores
+    }
     cd ..
 }
 
 function compareSha1sAndAnnotate([string]$module, [string]$version) {
-    if ((Get-Content ([string]::Format("../{0}_{1}_sha1.txt",$module,$version))) -eq (Get-Content ([string]::Format("{0}_{1}_sha1.txt",$module,$version)))){
-	Set-Variable -Name "new_sha1" -Value (Get-Content ([string]::Format("{0}_{1}_sha1.txt",$module,$version)))
-	Set-Variable -Name "old_sha1" -Value (Get-Content ([string]::Format("../{0}_{1}_sha1.txt",$module,$version)))
-	
-	if ($new_sha1 -ne $old_sha1){
-	    python qmlbenchrunner/annotate.py --title="$module update" --tag="$moduleUpdate" --text="Updated $module to $new_sha1 (previous was $old_sha1)" --branch="$version"
-	}
-	}
+    if ((Get-Content ([string]::Format("../{0}_{1}_sha1.txt", $module, $version))) -eq (Get-Content ([string]::Format("{0}_{1}_sha1.txt", $module, $version)))) {
+        Set-Variable -Name "new_sha1" -Value (Get-Content ([string]::Format("{0}_{1}_sha1.txt", $module, $version)))
+        Set-Variable -Name "old_sha1" -Value (Get-Content ([string]::Format("../{0}_{1}_sha1.txt", $module, $version)))
 
-    if ((Get-Content ([string]::Format("{0}_{1}_sha1.txt",$module,$version)))){
-	cp ([string]::Format("{0}_{1}_sha1.txt",$module,$version)) ([string]::Format("../{0}_{1}_sha1.txt",$module,$version))
+        if ($new_sha1 -ne $old_sha1) {
+            python qmlbenchrunner/annotate.py --title="$module update" --tag="$moduleUpdate" --text="Updated $module to $new_sha1 (previous was $old_sha1)" --branch="$version"
+        }
+    }
+
+    if ((Get-Content ([string]::Format("{0}_{1}_sha1.txt", $module, $version)))) {
+        cp ([string]::Format("{0}_{1}_sha1.txt", $module, $version)) ([string]::Format("../{0}_{1}_sha1.txt", $module, $version))
     }
 }
 
-Set-Variable -Name "branch_label" -Value ([string]::Format("{0}+{1}",$QtVersion,$QtDeclarativeVersion))
+Set-Variable -Name "branch_label" -Value ([string]::Format("{0}+{1}", $QtVersion, $QtDeclarativeVersion))
 Set-Variable -Name "qtdeclarative_branch" -Value $QtDeclarativeVersion
-if ($qtdeclarative_branch.length -le 0){
-    $qtdeclarative_branch=$QtVersion
-    $branch_label=$QtVersion
+if ($qtdeclarative_branch.length -le 0) {
+    $qtdeclarative_branch = $QtVersion
+    $branch_label = $QtVersion
 }
 
 echo "Using $QtVersion as base and $qtdeclarative_branch for qtdeclarative. Using $branch_label as label in database."
@@ -60,9 +61,9 @@ echo "Using $QtVersion as base and $qtdeclarative_branch for qtdeclarative. Usin
 pushd 'C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\'
 cmd /c "VsDevCmd.bat&set" |
 foreach {
-  if ($_ -match "=") {
-    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-  }
+    if ($_ -match "=") {
+        $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+    }
 }
 
 $env:Path += ";$FlexBisonDir"
@@ -97,8 +98,8 @@ cd ../..
 
 cd qmlbench
 
-if (Test-Path env:BADTESTS){
-	Remove-Item $env:BADTESTS -Recurse -Force
+if (Test-Path env:BADTESTS) {
+    Remove-Item $env:BADTESTS -Recurse -Force
 }
 
 src/release/qmlbench.exe --json --shell frame-count benchmarks/auto/creation/ benchmarks/auto/changes/ benchmarks/auto/js benchmarks/auto/animations benchmarks/auto/bindings > ../results.json
@@ -106,7 +107,7 @@ cd ..
 echo Label: $branch_label
 python qmlbenchrunner/run.py results.json $branch_label $MachineName
 
-if ($Annotate){
+if ($Annotate) {
     compareSha1sAndAnnotate qtbase $QtVersion
     compareSha1sAndAnnotate qtdeclarative $QtVersion
     compareSha1sAndAnnotate qtquickcontrols $QtVersion
@@ -120,4 +121,5 @@ rm -PATH qtdeclarative -Recurse -Force
 rm -PATH qtquickcontrols -Recurse -Force
 rm -PATH qtquickcontrols2 -Recurse -Force
 rm -PATH qtgraphicaleffects -Recurse -Force
+rm -PATH qttools -Recurse -Force
 rm -PATH qmlbench -Recurse -Force
